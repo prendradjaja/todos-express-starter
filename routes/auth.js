2179751484111328cb6adff5ba27ec278e0fd8e1
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2024-09-07 16:56:55 -0700

    2. Redirect to Google (add broken /login/federated/google, add passport-google-oidc)
    According to the docs (and as I'll probably see shortly) that route will
    be fixed by 1) configuring the strategy and 2) registering the app with
    Google

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var express = require('express');
+var passport = require('passport');
+var GoogleStrategy = require('passport-google-oidc');
+var db = require('../db');
 
 var router = express.Router();
 
 router.get('/login', function(req, res, next) {
   res.render('login');
 });
+
+router.get('/login/federated/google', passport.authenticate('google'));
 
 module.exports = router;
